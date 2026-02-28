b7b17ef23ddfdf3ff8215112793731f5239f9d12
rmandell06 <robertmandell237@gmail.com>
2026-02-28 00:55:29 -0600

    Update Bucket zone position in Debrief component

--- src/pages/Debrief.tsx
+++ src/pages/Debrief.tsx
@@ -76,7 +76,7 @@ export default function Debrief() {
   };
 
   const zones = [
-    { label: 'Bucket',      ids: ['1.7'],                              top: '0%', right: '0%'  },
+    { label: 'Bucket',      ids: ['1.7'],                              top: '50%', right: '50%'  },
     { label: 'Arm/Stick',   ids: ['1.9', '1.10'],                     top: '0%', right: '0%' },
     { label: 'Boom',        ids: ['1.5', '1.8'],                      top: '0%', left: '0%'  },
     { label: 'Cab',         ids: ['4.1','4.2','4.3','4.4','4.5','4.6','4.7','4.8','4.9','3.2','3.3','3.4'], top: '0%', left: '0%' },
